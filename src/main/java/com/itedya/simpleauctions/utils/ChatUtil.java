1847161a0bfff29916e5934c4d7f20924856bea3
itedya <bdomanski@protonmail.com>
2022-08-02 20:06:20 +0200

    Added NO_PERMISSION and YOU_HAVE_TO_BE_IN_GAME constants to Chat Utility

--- src/main/java/com/itedya/simpleauctions/utils/ChatUtil.java
+++ src/main/java/com/itedya/simpleauctions/utils/ChatUtil.java
@@ -1,9 +1,21 @@
 package com.itedya.simpleauctions.utils;
 
-import org.bukkit.ChatColor;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
 
 public class ChatUtil {
+    public final static BaseComponent[] NO_PERMISSION = new ComponentBuilder()
+            .color(ChatColor.RED)
+            .append("Brak permisji!")
+            .bold(true)
+            .create();
     public final static String UNKNOWN_COMMAND = ChatUtil.p("&e&lPodaj komendę! Ich spis możesz zobaczyć pod /licytacje pomoc");
+    public final static BaseComponent[] YOU_HAVE_TO_BE_IN_GAME = new ComponentBuilder()
+            .color(ChatColor.RED)
+            .append("Musisz być w grze, aby używać tej komendy!")
+            .bold(true)
+            .create();
 
     public static String p(String str) {
         return ChatColor.translateAlternateColorCodes('&', str);
