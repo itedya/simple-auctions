008058c3ea81de3fef7fd30b8b7999ba0d551f4d
itedya <bdomanski@protonmail.com>
2022-08-03 15:32:30 +0200

    Added create method to AuctionDao

--- src/main/java/com/itedya/simpleauctions/daos/AuctionDao.java
+++ src/main/java/com/itedya/simpleauctions/daos/AuctionDao.java
@@ -1,10 +1,30 @@
 package com.itedya.simpleauctions.daos;
 
 import com.itedya.simpleauctions.dtos.AuctionDto;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
 
+import java.sql.Date;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.UUID;
 
 public class AuctionDao {
-    private static List<AuctionDto> data = new ArrayList();
+    private final static List<AuctionDto> data = new ArrayList();
+
+    public static AuctionDto create(Player seller, Material material, int quantity) {
+        AuctionDto auctionDto = new AuctionDto();
+        auctionDto.uuid = UUID.randomUUID().toString();
+        auctionDto.buyerUUID = null;
+        auctionDto.sellerUUID = seller.getUniqueId().toString();
+        auctionDto.material = material;
+        auctionDto.quantity = quantity;
+        auctionDto.createdAt = new Date(Calendar.getInstance().getTime().getTime());
+        auctionDto.updatedAt = auctionDto.createdAt;
+        auctionDto.deletedAt = null;
+        data.add(auctionDto);
+
+        return auctionDto;
+    }
 }
