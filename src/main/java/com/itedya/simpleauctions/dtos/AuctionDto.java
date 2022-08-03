162f45a96466b47689577df76ef34b4a65382ac0
itedya <bdomanski@protonmail.com>
2022-08-03 15:31:03 +0200

    Added material and quantity props to AuctionDto

--- src/main/java/com/itedya/simpleauctions/dtos/AuctionDto.java
+++ src/main/java/com/itedya/simpleauctions/dtos/AuctionDto.java
@@ -1,11 +1,15 @@
 package com.itedya.simpleauctions.dtos;
 
+import org.bukkit.Material;
+
 import java.sql.Date;
 
 public class AuctionDto implements Dto {
     public String uuid;
     public String sellerUUID;
     public String buyerUUID;
+    public Material material;
+    public int quantity;
     public Date createdAt;
     public Date updatedAt;
     public Date deletedAt;
