58ca57a95fd0314f202ef98c9add048cb4eeb883
itedya <bdomanski@protonmail.com>
2022-08-03 16:08:02 +0200

    Finished CreateAuctionRunnable

--- src/main/java/com/itedya/simpleauctions/runnables/CreateAuctionRunnable.java
+++ src/main/java/com/itedya/simpleauctions/runnables/CreateAuctionRunnable.java
@@ -1,15 +1,17 @@
 package com.itedya.simpleauctions.runnables;
 
 import com.itedya.simpleauctions.daos.AuctionDao;
-import com.itedya.simpleauctions.dtos.AuctionDto;
+import com.itedya.simpleauctions.utils.ChatUtil;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class CreateAuctionRunnable extends BukkitRunnable {
-    private Player executor;
-    private Material material;
-    private int quantity;
+    private final Player executor;
+    private final Material material;
+    private final int quantity;
 
     public CreateAuctionRunnable(Player player, Material material, int quantity) {
         this.executor = player;
@@ -20,8 +22,28 @@ public class CreateAuctionRunnable extends BukkitRunnable {
     @Override
     public void run() {
         try {
-            AuctionDto auctionDto = AuctionDao.create(executor, material, quantity);
+            Inventory inventory = executor.getInventory();
+
+            boolean quantityCheck = inventory.containsAtLeast(new ItemStack(material), quantity);
+
+            if (!quantityCheck) {
+                executor.sendMessage(ChatUtil.p("&cNie masz tylu sztuk przedmiotu na sprzedaż!"));
+                return;
+            }
+
+            AuctionDao.create(executor, material, quantity);
+
+            int size = AuctionDao.getSize();
+
+            String message = "&a&lDodano przedmiot na sprzedaż!";
+
+            if (size != 1) {
+                message += "Jesteś %d w kolejce".formatted(AuctionDao.getSize() - 1);
+            }
+
+            executor.sendMessage(message);
         } catch (Exception e) {
+            executor.sendMessage(ChatUtil.SERVER_ERROR);
             e.printStackTrace();
         }
     }
