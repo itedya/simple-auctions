3eea5d3363e258ad8789e7d6a2cfddd0206b6ab7
itedya <bdomanski@protonmail.com>
2022-08-05 17:14:45 +0200

    Added VaultAPI economy to SimpleAuctions file

--- src/main/java/com/itedya/simpleauctions/SimpleAuctions.java
+++ src/main/java/com/itedya/simpleauctions/SimpleAuctions.java
@@ -7,6 +7,8 @@ import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.java.JavaPluginLoader;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.plugin.RegisteredServiceProvider;
 
 import java.io.File;
 
@@ -25,9 +27,17 @@ public final class SimpleAuctions extends JavaPlugin {
         return instance;
     }
 
+    private static Economy econ = null;
+
     @Override
     public void onEnable() {
         instance = this;
+
+        if (!setupEconomy()) {
+            this.getLogger().severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
 
         try {
             PluginCommand command = this.getCommand("licytacje");
@@ -39,6 +49,22 @@ public final class SimpleAuctions extends JavaPlugin {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+
+    public static Economy getEconomy() {
+        return econ;
     }
 
     @Override
