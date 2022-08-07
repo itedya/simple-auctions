93e2cf68d861fea700dbe090fd8618ac63c543dc
itedya <bdomanski@protonmail.com>
2022-08-07 16:11:08 +0200

    Added ItemPersistenceDto

---
+++ src/main/java/com/itedya/simpleauctions/dtos/ItemPersistenceDto.java
@@ -0,0 +1,9 @@
+package com.itedya.simpleauctions.dtos;
+
+import org.bukkit.Material;
+
+public class ItemPersistenceDto {
+    public String uuid;
+    public Material material;
+    public int quantity;
+}
